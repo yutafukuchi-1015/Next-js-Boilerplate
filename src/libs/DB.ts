cbb70d93bcee2ab95487499cda539fbc7d304d7d
yuuyuu <fufufu10155@gmail.com>
2025-07-24 00:17:44 +0900

    feat(db): connect prism pg

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,36 +1,11 @@
-import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import path from 'node:path';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { Pool } from 'pg';
 import * as schema from '@/models/Schema';
 import { Env } from './Env';
 
-// Stores the db connection in the global scope to prevent multiple instances due to hot reloading with Next.js
-const globalForDb = globalThis as unknown as {
-  drizzle: NodePgDatabase<typeof schema>;
-};
-
-// Need a database for production? Check out https://www.prisma.io/?via=nextjsboilerplate
-// Tested and compatible with Next.js Boilerplate
-const createDbConnection = () => {
-  return drizzle({
-    connection: {
-      connectionString: Env.DATABASE_URL,
-      ssl: !Env.DATABASE_URL.includes('localhost') && !Env.DATABASE_URL.includes('127.0.0.1'),
-    },
-    schema,
-  });
-};
-
-const db = globalForDb.drizzle || createDbConnection();
-
-// Only store in global during development to prevent hot reload issues
-if (Env.NODE_ENV !== 'production') {
-  globalForDb.drizzle = db;
-}
-
-await migrate(db, {
-  migrationsFolder: path.join(process.cwd(), 'migrations'),
+const pool = new Pool({
+  connectionString: Env.DATABASE_URL,
 });
+const db = drizzle({ client: pool, schema });
 
 export { db };
